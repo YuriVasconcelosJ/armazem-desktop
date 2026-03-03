11750137ee8cea88b08d4900a8b3c65a849a380c
Felipe Sabino de Oliveira <fsabino006@gmail.com>
2026-03-03 18:28:41 -0300

    style: adicionado trecho que aplica estilos ao sistema

--- src/main/java/br/com/project/config/ScreenManager.java
+++ src/main/java/br/com/project/config/ScreenManager.java
@@ -6,40 +6,39 @@ import javafx.stage.Stage;
 
 public class ScreenManager {
 
-    private static Stage stage;
+	private static Stage stage;
 
-    public static void setStage(Stage primaryStage) {
-        stage = primaryStage;
-    }
+	public static void setStage(Stage primaryStage) {
+		stage = primaryStage;
+	}
 
-    public static void switchScene(String fxml) {
+	public static void switchScene(String tela) {
 
-        try {
-            FXMLLoader loader = new FXMLLoader(
-                    ScreenManager.class.getResource(fxml)
-            );
+		try {
+			FXMLLoader loader = new FXMLLoader(ScreenManager.class.getResource(tela));
 
-            loader.setControllerFactory(type -> {
+			loader.setControllerFactory(type -> {
 
-               try {
+				try {
 //            	   tenta buscar no container
-            	   return ApplicationContext
-            			   .getInstance()
-            			   .getBean(type);
-               } catch (Exception e) {
-            	   try {
-            		   return type.getDeclaredConstructor().newInstance();
-            	   } catch (Exception exc) {
-            		   throw new RuntimeException(exc);
-            	   }            	   
-               }
-            });
+					return ApplicationContext.getInstance().getBean(type);
+				} catch (Exception e) {
+					try {
+						return type.getDeclaredConstructor().newInstance();
+					} catch (Exception exc) {
+						throw new RuntimeException(exc);
+					}
+				}
+			});
 
-            stage.setScene(new Scene(loader.load()));
-            stage.show();
+			Scene scene = new Scene(loader.load());
+			scene.getStylesheets().add(ScreenManager.class.getResource("/css/themes.css").toExternalForm());
 
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
-    }
+			stage.setScene(scene);
+			stage.show();
+
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+	}
 }
